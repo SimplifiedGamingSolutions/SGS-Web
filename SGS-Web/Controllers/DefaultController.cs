1c3bfa21a9ac9a3cb9cdd8bff0de7866d0572d91
Russell McDonnell <Russell.McDonnell@wavetronix.com>
2015-05-15 16:54:16 -0600

    Modified the layout and some of the structure of the website.

--- SGS-Web/Controllers/DefaultController.cs
+++ SGS-Web/Controllers/DefaultController.cs
@@ -15,14 +15,14 @@ namespace SGS_Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Simplified Gaming Solutions started in Utah by a few gamers who wanted to share their love for gaming with the world.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Here at Simplifed gaming solutions we want to make gamming easy to enjoy. If you have experienced any problems or have any concerns please feel free to call or Email us.";
 
             return View();
         }
